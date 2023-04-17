7b76e4f0459e71891bfefe41993456a9a94abc33
nina20126 <nina20126@student.hamk.fi>
2023-04-17 08:40:05 +0300

    moved Small python programs to own repository called Developing python applications

--- small python programs/square_root_function.py
+++
@@ -1,8 +0,0 @@
-'''
-Calculates the square root of value 2 (create your own function).
-'''
-
-def square_root(arr):
-    value_2 = arr[1]
-    squeare = value_2 ** (1/2)
-    return f"Square root of {value_2} is {squeare}"
